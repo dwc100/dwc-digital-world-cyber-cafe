e0a5134a45be4c0fcb280e7ed67fa5cb8c946f56
sudhanshu kumar <sudhanshukumarkumar2025@gmail.com>
2025-12-18 18:56:28 +0530

    Enhance main.py with print statements and calculations
    Added various print statements and arithmetic operations to demonstrate variable usage and types.

--- main.py
+++ main.py
@@ -1,3 +1,60 @@
 print ("hello world")
-print ('10+10')
-print ('10')
+print (10+10)
+print (10)
+print (12*2)
+print (3/6)
+name = "sudhanshu kumar"
+age = 18
+prise = 100
+print ("what is name:", name)
+print ("my age is:", age)
+print ("what is prise:", prise)
+age = 20
+age2 = age
+print  (age2)
+print (type (age))
+print (type (name))
+print (type (prise))
+name1 = "vinayak kumar"
+name2 = "sudhanshu kumar"
+name3 = ""'santosh singh'""
+print(name1)
+print(name2)
+print(name3)
+print (400+400)
+old = False
+a = None
+print(type(old))
+print(type(a))
+ 
+a = 20
+b = 30
+sum = a+b
+print(sum)
+a = 10
+b = 5
+diff = a/b
+print(diff)
+
+a = 10
+b = 5
+diff = a*b
+print(diff)
+
+a = 10
+b = 5
+diff = a-b
+print(diff)
+
+
+a = 22
+b = 7
+diff = a/b
+print (diff)
+
+
+a = 0.0641399416909621
+b = 7
+diff = a/b
+
+print (diff)
